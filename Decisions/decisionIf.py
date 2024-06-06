0394c068187be225e7bed74b8735b28c2ae5f896
macedolg <dev.macedolg@gmail.com>
2024-06-06 09:54:46 -0300

    [Finish] finalização de projeto [decisionif.py].

---
+++ Decisions/decisionIf.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if age > 65:
+    print("The patient" + name + " have priority service.")
+else:
+    print("The patient " + name + " DON'T have priority service.")
